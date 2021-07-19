1edcfbac5915d2354e35c75206d26144aa5bb76e
Marc Demoustier <marc.demoustier@epita.fr>
2021-07-19 17:28:12 +0200

    add simple argument parser

--- src/bw-build.py
+++ src/bw-build.py
@@ -1,1 +1,13 @@
-# FIXME
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Apply the Burrow-Wheeler transformation on the infile and output the '
+                                               'transformation in the outfile')
+    parser.add_argument('infile', action='store', help='path to the fasta input file')
+    parser.add_argument('outfile', action='store', help='path to the output file')
+    parser.add_argument('f', action='store', help='define the index creation frequency', type=int)
+    parser.add_argument('--compress', help='save the compressed version of the Burrow-Wheeler transformation',
+                        action='store_true')
+    return parser.parse_args()
+
+print(parse_arguments())
