538944bd45fd6036a31dd621a24787f62424a7af
Marc Demoustier <marc.demoustier@epita.fr>
2021-07-20 11:56:17 +0200

    add burrow wheeler transform function

--- src/bw-build.py
+++ src/bw-build.py
@@ -19,26 +19,37 @@ def read_infile(infile):
         # process the file
         line = input_file.readline()
         while (line):
-            line = input_file.readline()[:-1]
+            line = input_file.readline()[:-1] # remove the \n at the end of each line
             sequence += line
     return sequence
 
 
-def generate_outfile(args, sequence):
+def burrow_wheeler_transform(sequence):
+    sequence += '$'
+
+    table = sorted(sequence[i:] + sequence[:i] for i in range(len(sequence)))  # Table of rotations of string
+    last_column = [row[-1:] for row in table]  # Last characters of each row
+    return "".join(last_column)
+
+
+def generate_outfile(args, bw):
     c = 1 if args.compress else 0
-    n = 0 # FIXME: $ position
+    n = bw.index('$') if args.compress else 0
     p = 0 # FIXME: A adds at the end of the transform with compression for being a multiple of 4
     f = args.f
 
-    print(c, n, p, f)
-    print(sequence)
+    with open(args.outfile, 'w') as output_file:
+        output_file.write(f'{c} {n} {p} {f}\n')
+        output_file.write('0\n')
+        output_file.write(burrow_wheeler_transform(bw))
 
 
 def bw_build():
     args = parse_arguments()
 
     sequence = read_infile(args.infile)
-    generate_outfile(args, sequence)
+    bw = burrow_wheeler_transform(sequence)
+    generate_outfile(args, bw)
 
 
 if __name__ == "__main__":
