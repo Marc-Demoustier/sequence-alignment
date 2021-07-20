fad0542c022523ab6ca69c64dcc8d74f14aa2940
Marc Demoustier <marc.demoustier@epita.fr>
2021-07-20 10:45:38 +0200

    rename transformation in transform

--- src/bw-build.py
+++ src/bw-build.py
@@ -1,12 +1,12 @@
 import argparse
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Apply the Burrow-Wheeler transformation on the infile and output the '
+    parser = argparse.ArgumentParser(description='Apply the Burrow-Wheeler transform on the infile and output the '
                                                'transformation in the outfile')
     parser.add_argument('infile', action='store', help='path to the fasta input file')
     parser.add_argument('outfile', action='store', help='path to the output file')
     parser.add_argument('f', action='store', help='define the index creation frequency', type=int)
-    parser.add_argument('--compress', help='save the compressed version of the Burrow-Wheeler transformation',
+    parser.add_argument('--compress', help='save the compressed version of the Burrow-Wheeler transform',
                         action='store_true')
     return parser.parse_args()
 
