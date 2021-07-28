dee19094b7f05bfe7c6f5827d2507161b2d7fb25
Marc Demoustier <marc.demoustier@epita.fr>
2021-07-28 20:15:53 +0200

    compute progressive bwt build

--- src/bw-build.py
+++ src/bw-build.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Apply the Burrow-Wheeler transform on the infile and output the '
@@ -8,8 +9,8 @@ def parse_arguments():
     parser.add_argument('f', action='store', help='define the index creation frequency', type=int)
     parser.add_argument('--compress', help='save the compressed version of the Burrow-Wheeler transform',
                         action='store_true')
-    parser.add_argument('--progressive', help='create the index with a progressive version using parameter k',
-                        action='store_true')
+    parser.add_argument('--progressive', help='create the index with a progressive version using parameter k', type=int,
+                        action='store', metavar='k')
     return parser.parse_args()
 
 
@@ -24,16 +25,38 @@ def read_infile(infile):
     return sequence
 
 
-def burrow_wheeler_transform(sequence, frequency):
+def burrow_wheeler_transform(sequence, frequency, k):
     sequence += '$'
 
-    table = sorted(sequence[i:] + sequence[:i] for i in range(len(sequence)))  # Table of rotations of string
+    if k:
+        bwt = ''
+        positions_index = []
 
-    # create list of indexes (position of $)
-    positions_index = [str(len(table) - table[i].index('$') - 1) for i in range(0, len(table), frequency)]
+        # create splitters array
+        splitters = sorted(sequence[i:] + sequence[:i] for i in random.sample(range(len(sequence)), k))
+        splitters.insert(0, chr(0))
+        splitters.append(chr(127))
 
-    last_column = [row[-1:] for row in table]  # Last characters of each row
-    return "".join(last_column), positions_index
+        for i in range(len(splitters) - 1):
+            bucket = []
+            for j in range(len(sequence)):
+                rotation = sequence[j:] + sequence[:j]
+                if splitters[i] <= rotation < splitters[i + 1]:
+                    bucket.append(rotation)
+            bucket = sorted(bucket)
+            last_column = [row[-1:] for row in bucket]  # Last characters of each row
+            bwt += ''.join(last_column)
+
+    else:
+        table = sorted(sequence[i:] + sequence[:i] for i in range(len(sequence)))  # Table of rotations of string
+        last_column = [row[-1:] for row in table]  # Last characters of each row
+        bwt = ''.join(last_column)
+
+        # create list of indexes (position of $)
+        positions_index = [str(len(table) - table[i].index('$') - 1) for i in range(0, len(table), frequency)]
+
+    print(bwt)
+    return bwt, positions_index
 
 
 def compress_sequence(bw, p):
@@ -59,7 +82,7 @@ def compress_sequence(bw, p):
 
 
 def generate_outfile(args, sequence):
-    bw, positions_index = burrow_wheeler_transform(sequence, args.f)
+    bw, positions_index = burrow_wheeler_transform(sequence, args.f, args.progressive)
 
     c = 1 if args.compress else 0
     n = bw.index('$') if args.compress else 0
@@ -84,7 +107,6 @@ def generate_outfile(args, sequence):
 
 def bw_build():
     args = parse_arguments()
-
     sequence = read_infile(args.infile)
     generate_outfile(args, sequence)
 
